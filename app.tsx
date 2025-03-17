5debfccd7ea63da07581aef895329adcee36ca0e
Nils Falt <nils.faelt@gmail.com>
2025-03-17 19:36:08 +0100

    build

---
+++ app.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import ReactDOM from "react-dom/client";
+import React from "react";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <button onClick={() => setCount(count + 1)}></button>
+      <h2>{count}</h2>
+      <h1>Im built</h1>
+    </div>
+  );
+};
+root.render(<App />);
